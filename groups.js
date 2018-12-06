7b6baa4e108908412f1f81a90309faa8d15787e0
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-12-05 21:42:15 -0500

    Updated the addition of new equations to the group

--- groups.js
+++ groups.js
@@ -7,14 +7,27 @@ exports.OPERATION_KEY = OPERATION_KEY;
 exports.JOIN_NUMBERS = JOIN_NUMBERS;
 exports.NUMBER_KEY = NUMBER_KEY;
 
+function addToGroup(equation, group) {
+  for (var i = 0; i < group.length; i ++) {
+    if (group[i].toString() == equation.toString()) {
+      return group;
+    }
+  }
+
+  group.push(equation.slice(0));
+
+  return group;
+}
+
+
 function addGroupNumberEquations(equation, group) {
   if (equation.length != 1) {
     if (equation[0] == 1) {
-      group.push(equation.slice(0));
+      group = addToGroup(equation.slice(0), group);
     } else {
       for (let i = 0; i < equation.length; i ++) {
         equation.push(equation.shift())
-        group.push(equation.slice(0));
+        group = addToGroup(equation.slice(0), group);
       }   
     }
   }
@@ -68,27 +81,10 @@ function createGroupNumber(number) {
   return group;
 }
 
-function filterGroupNumber(number, group) {
-  let equationStrings = group.map(function(equation) {
-    return equation.join(",");
-  });
-
-  equationStrings = equationStrings.filter(function (equation, index, equationStrings) {
-    return equationStrings.lastIndexOf(equation) === index;
-  });
-
-  return equationStrings.map(function(equationString) {
-    return equationString.split(",").map(function(number) {
-      return parseInt(number);
-    });
-  });
-}
-
 exports.generate = function (number) {
   let groups = {};
   for (let i = 2; i <= number; i ++) {
     groups[i] = createGroupNumber(i);
-    groups[i] = filterGroupNumber(i, groups[i]);
   }
 
   return groups;
